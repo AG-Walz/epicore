e819c9b3d34838decf49cb185abd669368b121fc
Jana Hoffmann <jana@family-hoffmann.de>
2024-11-14 14:53:47 +0100

    add params file and remove command line parameter

--- plateau_main.py
+++ plateau_main.py
@@ -4,56 +4,16 @@ import shutil
 import pandas as pd 
 import ast
 
+from bin.params import *
 from bin.compute_cores import gen_epitope
 from bin.map_result import map_pep_core
 from bin.visualize_protein import vis_prot
 from bin.parse_input import parse_input
-    
+
 def __main__():
-
-    parser = argparse.ArgumentParser(description='Input File and Parameters')
-    parser.add_argument('-input_tsv', type=str,
-                        help='PATH to the identification data.')
-    parser.add_argument('-proteome', type=str, 
-                        help='PATH to the fasta file of the proteome used for identification.')
-    parser.add_argument('-min_epi_length', type=int, required=False, default=9,
-                        help='Minimum epitope length. If no core is found for that length the core next longest core gets chosen.')
-    parser.add_argument('-max_step_size', type=int, required=False, default=5,
-                        help='The maximum step size between two epitopes to still be grouped together.')
-    parser.add_argument('-min_overlap', type=int, required=False, default=11,
-                        help='The minimum overlap between two epitopes to still be grouped together.')
-    parser.add_argument('-prot_accession', type=str, required=False,
-                        help='Accessions of proteins for which peptide coverage and core epitopes should be visualized. The input should be a string with comma separated accessions.')
-    parser.add_argument('-out', type=str, required=True,
-                        help='Path to where output tsvs and pdfs should be stored.')
-    parser.add_argument('-plateau_csv', type=str, required=False,
-                        help='Path to localplateau tsv. If set all input arguments other than -prot_accession, -out and -plateau_csv will be ignored and the peptide coverage and core epitopes of the given proteins will get visualized based on the plateau_csv.')
-    parser.add_argument('-seq_column', type=str, required=True,
-                        help='Header of the input data column, that contains the peptide sequences.')
-    parser.add_argument('-protacc_column', type=str, required=True,
-                        help='Header of the input data column, that contains the protein accession.')
-    parser.add_argument('-pep_position', type=str, required=False,default='',
-                        help='Comma separated string, with the headers of the input data columns, that contain the peptide start and end position in the protein. This is an optional flag and will speed up the process a lot.')
-    parser.add_argument('-delimiter', type=str, required=False,default=None,
-                        help='Delimiter of entries in columns of input file.')
-    parser.add_argument('-mod_delimiter', type=str, required=True,
-                        help='Delimiter that separates the peptide sequence from modifications.')
-    args = parser.parse_args()
-
-    mhcquant_out = args.input_tsv
-    fasta_proteome = args.proteome
-    min_epi_length = args.min_epi_length
-    min_overlap = args.min_overlap
-    max_step_size = args.max_step_size
-    prot_accession = args.prot_accession
-    out_dir = args.out 
-    plateau_csv = args.plateau_csv
-    seq_column = args.seq_column
-    protacc_column = args.protacc_column
-    pep_position = args.pep_position
-    delimiter = args.delimiter
-    mod_delimiter = args.mod_delimiter
     
+    pep_position = ''
+
     # check if all input paths exist
     if not os.path.isfile(mhcquant_out):
         raise Exception('The given evidence file does not exist.')
